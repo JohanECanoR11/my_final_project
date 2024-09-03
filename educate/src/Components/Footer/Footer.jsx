bd4f9befd18b2b555b7aa43122e9b5c01c89ff99
Johan E Cano Ruiz <johan.memincho@gmail.com>
2024-09-02 23:41:10 -0500

    configuring footer ok

---
+++ educate/src/Components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <p>© 2024 Edúcate. Todos los derechos reservados</p>
+      <ul>
+        <li>Condiciones de servicio</li>
+        <li>Política de Privacidad</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Footer
--- educate/src/Components/Navbar/Navbar.jsx
+++ educate/src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
-
+import { Link } from 'react-scroll';
 
 const Navbar = () => {
 
@@ -17,12 +17,12 @@ const Navbar = () => {
     <nav className={`container ${sticky? 'dark-nav' : ''}`}>
       <img src={logo} alt="logo" className='logo' />
       <ul>
-        <li>Inicio</li>
-        <li>Programa</li>
-        <li>¿Quiénes Somos?</li>
-        <li>Campus</li>
-        <li>Testimonios</li>
-        <li><button className='btn'>Contáctanos</button></li>
+        <li><Link to='hero' smooth={true} offset={0} duration={500}>Inicio</Link></li>
+        <li><Link to='program' smooth={true} offset={-260} duration={500}>Programa</Link></li>
+        <li><Link to='about' smooth={true} offset={-150} duration={500}>¿Quiénes Somos?</Link></li>
+        <li><Link to='campus' smooth={true} offset={-260} duration={500}>Campus</Link></li>
+        <li><Link to='testimonials' smooth={true} offset={-260} duration={500}>Testimonios</Link></li>
+        <li><Link to='contact' smooth={true} offset={-260} duration={500} className='btn'>Contáctanos</Link></li>
       </ul>
     </nav>
   )
